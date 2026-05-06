47c9ff1837f816200df4415c5e54bc37cda3a94b
Duong Ngoc Khoi Nguyen <52300045@student.tdtu.edu.vn>
2026-05-06 09:55:18 +0700

    relax CORS for production

--- be/src/main.ts
+++ be/src/main.ts
@@ -4,12 +4,7 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: [
-      'http://localhost:3000',
-      'http://127.0.0.1:3000',
-      'http://localhost:3001',
-      'http://127.0.0.1:3001',
-    ],
+    origin: true,
   });
   await app.listen(process.env.PORT ?? 4000);
 }
